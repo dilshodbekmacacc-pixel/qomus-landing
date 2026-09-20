3e7b287b3f8de20862f0e34db92e7bfa941e1da5
dilshodbekmacacc-pixel <dilshodbek.mac.acc@gmail.com>
2026-09-20 17:28:47 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,62 +1,37 @@
 const translations = {
     uz_cyr: {
-        tickerText: "<span>🚚 Ўзбекистон бўйлаб мутлақо БЕПУЛ етказиб берамиз</span><span>🌿 100% Табиий маҳсулот</span><span>⭐ 20,000+ Манмун мижозлар</span><span>🔐 100% Махфийлик кафолатланади</span><span>🚚 Ўзбекистон бўйлаб мутлақо БЕПУЛ етказиб берамиз</span><span>🌿 100% Табиий маҳсулот</span><span>⭐ 20,000+ Манмун мижозлар</span><span>🔐 100% Махфийлик кафолатланади</span>",
+        tickerText: "<span>🚚 Ўзбекистон бўйлаб мутлақо БЕПУЛ етказиб берамиз</span><span>🌿 100% Табиий маҳсулот</span><span>⭐ 20,000+ Манмун мижозлар</span><span>🔐 100% Махфийлик кафолатланади</span>",
         navBtn: "Буюртма бериш",
         heroBadge: "⭐ 20,000+ Мижозлар Ишончи",
         heroTitle: "ОҚ ДОҒЛАРДАН ҲАЛОС БЎЛИШ ЭНДИ ИЛОЖСИЗ ЭМАС!",
-        heroDesc: "Ушбу препарат витилиго ва теридаги турли касалликларга ечим бўла олади. Ва ўзига ўргатиб қўймайди - Натижа кафолатланган!",
+        heroDesc: "Ушбу препарат витилиго ва теридаги турли касалликларга ечим бўла олади. Натижа кафолатланган!",
         feat1: "100% Табиий таркиб",
         feat2: "Ўзига ўргатиб қўймайди",
         feat3: "Кафолатланган натижа",
         heroBtn: "Буюртма бериш",
         subCta: "🔒 Маълумотларингиз сир тутилиши кафолатланади",
-        floatBtn: "Ҳозироқ Буюртма Бериш",
 
         painTitle: "Терингизни аввалги ҳолатига қайтаришнинг иложи йўқ деб ўйласангиз – Адашасиз!",
-        painIntro: "Сиз шу ергача ўқиётган бўлсангиз, демак, сизда ёки оилангизда ҳам витилиго ёки тери муаммолари бор:",
         pain1: "Теридаги оқ доғлар",
         pain2: "Уят ёки ноқулайлик ҳисси",
-        pain3: "Асабийлик ва ўз-ўзингизга ишончсизлик",
-        pain4: "Турли мазлар ва даволардан фойда кўрмагансиз ёки таъсири кам...",
-        painAgitation: "Айниқса, йилнинг иссиқ фаслларида ёки енги калта кийимлар кийганда ноқулайлик сезасиз... Витилигодан халос бўлишни хоҳлайсиз-у, лекин \"Қандай қилиб?\" деган саволга жавоб тополмай бошингиз қотган, тўғрими?",
-        warnTitle: "Аммо бу муаммоларга зудлик билан ечим топишингиз лозим!",
-        warnDesc: "Акс ҳолда, вақтида даволанмаса, витилиго бутун танани эгаллаб, тузатиб бўлмас ҳолатга келиб қолиши мумкин! Тасаввур қилишнинг ўзи даҳшатли.",
-        questionText: "Касалликга қарши курашасизми ёки йўқ?!",
-        faithText: "Ишончим комил, сиз бу муаммони енгасиз ва терингиз соғлом ҳолатига қайтади.",
-
-        solBadge: "Бизда витилигога ечим бор",
-        solTitle: "Бу – QOMUS табиий шифобахш комплекси.",
-        solDesc: "20 000+ беморларга бу муаммолардан халос бўлишда ёрдам берганмиз.",
-        solBtn: "Буюртма бериш",
+        pain3: "Асабийлик ва ишончсизлик",
+        pain4: "Натижасиз мазлар ва даволар",
 
         howMainTitle: "Препаратимизнинг бошқалардан афзаллиги",
-        howMainSub: "Ушбу 100% табиий гиёҳ ва ингредиентлардан тайёрланган. Маҳсулотимиз таркибидаги табиий моддалар организмга тушиб:",
-        step1P: "Биринчи ўринда меланин ишлаб чиқаришга фаол таъсир кўрсатади.",
-        step2P: "Ҳужайра алмашинувини яхшилайди ва тезлаштиради.",
-        step3P: "Ўлик ҳужайраларни тозалаб, янги ҳужайраларнинг нобуд бўлишига йўл қўймайди.",
-        step4P: "Асаб тизим ҳолатини нормага келтириб, уйқуни яхшилайди.",
+        step1P: "Меланин ишлаб чиқаришни фаоллаштиради.",
+        step2P: "Ҳужайра алмашинувини тезлаштиради.",
+        step3P: "Янги ҳужайраларни нобуд бўлишидан ҳимоя қилади.",
+        step4P: "Асаб тизимини ва уйқуни нормаллаштиради.",
         howBtn: "Буюртма бериш",
 
-        benTitle: "Айнан QOMUS воситасини танлаб нималарга эришасиз?",
-        benDesc: "Препарат курс бўйича қўлланилганда витилигони даволайди ва қайталанишининг олдини ҳам олади. Табиий ингредиентлардан тайёрланган Qомус терида меланин ишлаб чиқаришни фаоллаштириб, витилигодан халос қилади, организмни ҳимоя қилади ва янги доғларнинг пайдо бўлишининг олдини олади. Бу эса сизга ҳаётингиздан тўлақонли завқланиб, оғриқларсиз роҳат билан яшаш имкониятини беради.",
-        bp1T: "Алоҳида ва индивидуал ёндошув",
-        bp1D: "Айнан сиз билан алоҳида ишлаймиз, ҳолингиздан доимий хабар олиб турамиз (бошқалар сиздан хабар олмайди).",
-        bp2T: "2019-йилдан бери катта тажриба",
-        bp2D: "Qомус 2019 йилдан бери 20 000дан ортиқ витилиго билан оғриган беморларга ёрдам берди ва бу рақамлар кун сайин ошиб бормоқда.",
-        bp3T: "Мутлақо БЕПУЛ етказиб бериш",
-        bp3D: "Ўзимиз уйингизгача мутлақо БЕПУЛ етказиб берамиз (бошқалар пул эвазига етказиб беради).",
-        calloutText: "Агар ушбу ажойиб имкониятни қўлдан бой беришни истамасангиз, ҳозироқ қуйидаги тугмани босиб рўйхатдан ўтинг. Рўйхатдан ўтганингиздан сўнг сиз билан қисқа вақт ичида боғланамиз!",
-        benBtn: "Буюртма бериш",
+        condSubtitle: "Витилиго (оқ доғлар)га қарши Qomus",
+        condText: "Qомус таркибидаги моддалар меланин ишлаб чиқариш жараёнини фаоллаштиради ва теридаги пигмент танқислигини камайтиради. Мунтазам қўллаш натижасида терининг табиий ранги тикланади ва янги оқ доғлар пайдо бўлишининг олди олинади.",
 
-        condTitle: "Qomus орқали қандай касалликлардан қутуласиз?",
-        condSubtitle: "Витилиго (оқ доғлар)",
-        condText: "Витилиго – кўп ҳолларда ички аъзолар, айниқса, иммун тизими ва жигар билан боғлиқ муаммолар туфайли келиб чиқади. Qомус таркибидаги моддалар меланин ишлаб чиқариш жараёнини фаоллаштиради ва теридаги пигмент танқислигини камайтиради. Ушбу восита организмда витаминлар ва микроэлементлар танқислигини тўлдириб, тери ҳужайраларини тиклайди. Мунтазам қўллаш натижасида терининг табиий ранги тикланади ва янги оқ доғлар пайдо бўлишининг олди олинади.",
-
-        formTitle: "Бепул Мутахассис Консультациясини Олинг ва Буюртма Беринг",
-        formSubtitle: "Формани тўлдиринг, мутахассисимиз қисқа вақт ичида сиз билан боғланади.",
+        formTitle: "Бепул Консультация ва Буюртма",
+        formSubtitle: "Маълумотларингизни қолдиринг, мутахассисимиз сиз билан боғланади.",
         lblName: "Исмингиз:",
         lblPhone: "Телефон рақамингиз:",
-        lblQ: "Бу ҳолат қанча вақтдан бери кузатилмоқда?",
+        lblQ: "Қанча вақтдан бери кузатилмоқда?",
         opt1: "6 ойгача",
         opt2: "1 йилдан 3 йилгача",
         opt3: "3 йилдан ортиқ",
@@ -67,59 +42,34 @@ const translations = {
         navBtn: "Buyurtma berish",
         heroBadge: "⭐ 20,000+ Mijozlar Ishonchi",
         heroTitle: "OQ DOG'LARDAN HALOS BO'LISH ENDI ILOJSIZ EMAS!",
-        heroDesc: "Ushbu preparat vitiligo va teridagi turli kasalliklarga yechim bo'la oladi. Va o'ziga o'rgatib qo'ymaydi - Natija kafolatlangan!",
+        heroDesc: "Ushbu preparat vitiligo va teridagi turli kasalliklarga yechim bo'la oladi. Natija kafolatlangan!",
         feat1: "100% Tabiiy tarkib",
         feat2: "O'ziga o'rgatib qo'ymaydi",
         feat3: "Kafolatlangan natija",
         heroBtn: "Buyurtma berish",
         subCta: "🔒 Ma'lumotlaringiz sir tutilishi kafolatlanadi",
-        floatBtn: "Hoziroq Buyurtma Berish",
 
         painTitle: "Teringizni avvalgi holatiga qaytarishning iloji yo'q deb o'ylasangiz – Adashasiz!",
-        painIntro: "Siz shu yergacha o'qiyotgan bo'lsangiz, demak, sizda yoki oilangizda ham vitiligo yoki teri muammolari bor:",
         pain1: "Teridagi oq dog'lar",
         pain2: "Uyat yoki noqulaylik hissi",
-        pain3: "Asabiylik va o'z-o'zingizga ishonchsizlik",
-        pain4: "Turli mazlar va davolardan foyda ko'rmagansiz yoki ta'siri kam...",
-        painAgitation: "Ayniqsa, yilning issiq fasllarida yoki engi kalta kiyimlar kiyganda noqulaylik sezasiz... Vitiligodan xalos bo'lishni xohlaysiz-u, lekin \"Qanday qilib?\" degan savolga javob topolmay boshingiz qotgan, to'g'rimi?",
-        warnTitle: "Ammo bu muammolarga zudlik bilan yechim topishingiz lozim!",
-        warnDesc: "Aks holda, vaqtida davolanmasa, vitiligo butun tanani egallab, tuzatib bo'lmas holatga kelib qolishi mumkin! Tasavvur qilishning o'zi dahshatli.",
-        questionText: "Kasallikga qarshi kurashasizmi yoki yo'q?!",
-        faithText: "Ishonchim komil, siz bu muammoni engasiz va teringiz sog'lom holatiga qaytadi.",
-
-        solBadge: "Bizda vitiligoga yechim bor",
-        solTitle: "Bu – QOMUS tabiiy shifobaxsh kompleksi.",
-        solDesc: "20 000+ bemorlarga bu muammolardan xalos bo'lishda yordam berganmiz.",
-        solBtn: "Buyurtma berish",
+        pain3: "Asabiylik va ishonchsizlik",
+        pain4: "Natijasiz mazlar va davolar",
 
         howMainTitle: "Preparatimizning boshqalardan afzalligi",
-        howMainSub: "Ushbu 100% tabiiy giyoh va ingredientlardan tayyorlangan. Mahsulotimiz tarkibidagi tabiiy moddalar organizmga tushib:",
-        step1P: "Birinchi o'rinda melanin ishlab chiqarishga faol ta'sir ko'rsatadi.",
-        step2P: "Hujayra almashinuvini yaxshilaydi va tezlashtiradi.",
-        step3P: "O'lik hujayralarni tozalab, yangi hujayralarning nobud bo'lishiga yo'l qo'ymaydi.",
-        step4P: "Asab tizim holatini normaga keltirib, uyquni yaxshilaydi.",
+        step1P: "Melanin ishlab chiqarishni faollashtiradi.",
+        step2P: "Hujayra almashinuvini tezlashtiradi.",
+        step3P: "Yangi hujayralarni nobud bo'lishidan himoya qiladi.",
+        step4P: "Asab tizimini va uyquni normallashtiradi.",
         howBtn: "Buyurtma berish",
 
-        benTitle: "Aynan QOMUS vositasini tanlab nimalarga erishasiz?",
-        benDesc: "Preparat kurs bo'yicha qo'llanilganda vitiligoni davolaydi va qaytalanishining oldini ham oladi. Tabiiy ingredientlardan tayyorlangan Qomus terida melanin ishlab chiqarishni faollashtirib, vitiligodan xalos qiladi, organizmni himoya qiladi va yangi dog'larning paydo bo'lishining oldini oladi. Bu esa sizga hayotingizdan to'laqonli zavqlanib, og'riqlarsiz rohat bilan yashash imkoniyatini beradi.",
-        bp1T: "Alohida va individual yondashuv",
-        bp1D: "Aynan siz bilan alohida ishlaymiz, holingizdan doimiy xabar olib turamiz (boshqalar sizdan xabar olmaydi).",
-        bp2T: "2019-yildan beri katta tajriba",
-        bp2D: "Qomus 2019 yildan beri 20 000dan ortiq vitiligo bilan og'rigan bemorlarga yordam berdi va bu raqamlar kun sayin oshib bormoqda.",
-        bp3T: "Mutlaqo BEPUL yetkazib berish",
-        bp3D: "O'zimiz uyingizgacha mutlaqo BEPUL yetkazib beramiz (boshqalar pul evaziga yetkazib beradi).",
-        calloutText: "Agar ushbu ajoyib imkoniyatni qo'ldan boy berishni istamasangiz, hoziroq quyidagi tugmani bosib ro'yxatdan o'ting. Ro'yxatdan o'tganingizdan so'ng siz bilan qisqa vaqt ichida bog'lanamiz!",
-        benBtn: "Buyurtma berish",
+        condSubtitle: "Vitiligo (oq dog'lar)ga qarshi Qomus",
+        condText: "Qomus tarkibidagi moddalar melanin ishlab chiqarish jarayonini faollashtiradi va teridagi pigment tanqisligini kamaytiradi. Muntazam qo'llash natijasida terining tabiiy rangi tiklanadi va yangi oq dog'lar paydo bo'lishining oldi olinadi.",
 
-        condTitle: "Qomus orqali qanday kasalliklardan qutulasiz?",
-        condSubtitle: "Vitiligo (oq dog'lar)",
-        condText: "Vitiligo – ko'p hollarda ichki a'zolar, ayniqsa, immun tizimi va jigar bilan bog'liq muammolar tufayli kelib chiqadi. Qomus tarkibidagi moddalar melanin ishlab chiqarish jarayonini faollashtiradi va teridagi pigment tanqisligini kamaytiradi. Ushbu vosita organizmda vitaminlar va mikroelementlar tanqisligini to'ldirib, teri hujayralarini tiklaydi. Muntazam qo'llash natijasida terining tabiiy rangi tiklanadi va yangi oq dog'lar paydo bo'lishining oldi olinadi.",
-
-        formTitle: "Bepul Mutaxassis Konsultatsiyasini Oling va Buyurtma Bering",
-        formSubtitle: "Formani to'ldiring, mutaxassisimiz qisqa vaqt ichida siz bilan bog'lanadi.",
+        formTitle: "Bepul Konsultatsiya va Buyurtma",
+        formSubtitle: "Ma'lumotlaringizni qoldiring, mutaxassisimiz siz bilan bog'lanadi.",
         lblName: "Ismingiz:",
         lblPhone: "Telefon raqamingiz:",
-        lblQ: "Bu holat qancha vaqtdan beri kuzatilmoqda?",
+        lblQ: "Qancha vaqtdan beri kuzatilmoqda?",
         opt1: "6 oygacha",
         opt2: "1 yildan 3 yilgacha",
         opt3: "3 yildan ortiq",
@@ -130,56 +80,31 @@ const translations = {
         navBtn: "Оформить заказ",
         heroBadge: "⭐ Доверие более 20,000+ клиентов",
         heroTitle: "ИЗБАВЛЕНИЕ ОТ БЕЛЫХ ПЯТЕН ТЕПЕРЬ ВОЗМОЖНО!",
-        heroDesc: "Данный препарат станет решение при витилиго и различных кожных заболеваниях. Не вызывает привыкания - Результат гарантирован!",
+        heroDesc: "Данный препарат станет решением при витилиго и кожных заболеваниях. Результат гарантирован!",
         feat1: "100% Натуральный состав",
         feat2: "Не вызывает привыкания",
         feat3: "Гарантированный результат",
         heroBtn: "Оформить заказ",
         subCta: "🔒 Полная конфиденциальность ваших данных",
-        floatBtn: "Заказать прямо сейчас",
 
         painTitle: "Если вы думаете, что вернуть коже прежний вид невозможно – вы ошибаетесь!",
-        painIntro: "Если вы читаете это, значит, вы или ваши близкие столкнулись с витилиго или проблемами кожи:",
         pain1: "Белые пятна на коже",
         pain2: "Чувство стыда и дискомфорта",
-        pain3: "Раздражительность и неуверенность в себе",
-        pain4: "Различные мази не дали результатов...",
-        painAgitation: "Особенно в теплое время года, когда хочется носить открытую одежду... Вы хотите избавиться от витилиго, но не знаете, с чего начать?",
-        warnTitle: "Но эти проблемы требуют срочного решения!",
-        warnDesc: "Если не начать лечение вовремя, витилиго может распространиться по всему телу!",
-        questionText: "Готовы ли вы бороться с болезнью?!",
-        faithText: "Мы уверены, что вы справитесь, и ваша кожа вернет здоровый вид.",
-
-        solBadge: "У нас есть решение от витилиго",
-        solTitle: "Это – натуральный комплекс QOMUS.",
-        solDesc: "Мы помогли избавиться от проблемы более 20 000+ пациентам.",
-        solBtn: "Оформить заказ",
+        pain3: "Раздражительность и неуверенность",
+        pain4: "Бесполезные мази и средства",
 
         howMainTitle: "Преимущества нашего препарата",
-        howMainSub: "Изготовлен из 100% натуральных трав и ингредиентов. Компоненты препарата:",
-        step1P: "В первую очередь активно стимулируют выработку меланина.",
-        step2P: "Улучшают и ускоряют клеточный обмен.",
-        step3P: "Очищают отмершие клетки и предотвращают гибель новых.",
-        step4P: "Нормализуют работу нервной системы и улучшают сон.",
+        step1P: "Стимулирует выработку меланина.",
+        step2P: "Ускоряет клеточный обмен.",
+        step3P: "Защищает новые клетки от гибели.",
+        step4P: "Нормализует работу нервной системы.",
         howBtn: "Оформить заказ",
 
-        benTitle: "Чего вы достигнете, выбрав QOMUS?",
-        benDesc: "При курсовом приеме препарат помогает устранить витилиго и предотвращает его повторное появление. Натуральный Qomus активирует выработку меланина и защищает организм.",
-        bp1T: "Индивидуальный подход",
-        bp1D: "Мы работаем с вами индивидуально и постоянно сопровождаем вас.",
-        bp2T: "Большой опыт с 2019 года",
-        bp2D: "С 2019 года Qomus помог более 20 000 пациентов с витилиго.",
-        bp3T: "Совершенно БЕСПЛАТНАЯ доставка",
-        bp3D: "Мы доставим прямо до вашей двери абсолютно бесплатно.",
-        calloutText: "Не упустите эту возможность, оставьте заявку прямо сейчас!",
-        benBtn: "Оформить заказ",
+        condSubtitle: "Qomus против витилиго (белых пятен)",
+        condText: "Компоненты Qomus активируют синтез меланина и восполняют дефицит микроэлементов. При регулярном применении возвращается естественный цвет кожи.",
 
-        condTitle: "От каких проблем помогает избавиться Qomus?",
-        condSubtitle: "Витилиго (белые пятна)",
-        condText: "Витилиго чаще всего связано с нарушениями иммунной системы и печени. Компоненты Qomus активируют синтез меланина и восполняют дефицит микроэлементов.",
-
-        formTitle: "Получите бесплатную консультацию и оформите заказ",
-        formSubtitle: "Заполните форму, и наш специалист свяжется с вами в ближайшее время.",
+        formTitle: "Бесплатная Консультация и Заказ",
+        formSubtitle: "Оставьте данные, и наш специалист свяжется с вами.",
         lblName: "Ваше имя:",
         lblPhone: "Ваш номер телефона:",
         lblQ: "Как долго наблюдается это состояние?",
@@ -206,45 +131,20 @@ function changeLang(lang) {
     document.getElementById('feat-3').innerText = t.feat3;
     document.getElementById('hero-btn-text').innerText = t.heroBtn;
     document.getElementById('sub-cta-text').innerText = t.subCta;
-    document.getElementById('float-btn-text').innerText = t.floatBtn;
 
     document.getElementById('pain-title').innerText = t.painTitle;
-    document.getElementById('pain-intro').innerText = t.painIntro;
     document.getElementById('pain-1').innerText = t.pain1;
     document.getElementById('pain-2').innerText = t.pain2;
     document.getElementById('pain-3').innerText = t.pain3;
     document.getElementById('pain-4').innerText = t.pain4;
-    document.getElementById('pain-agitation').innerText = t.painAgitation;
-    document.getElementById('warn-title').innerText = t.warnTitle;
-    document.getElementById('warn-desc').innerText = t.warnDesc;
-    document.getElementById('question-text').innerText = t.questionText;
-    document.getElementById('faith-text').innerText = t.faithText;
-
-    document.getElementById('sol-badge').innerText = t.solBadge;
-    document.getElementById('sol-title').innerText = t.solTitle;
-    document.getElementById('sol-desc').innerText = t.solDesc;
-    document.getElementById('sol-btn').innerText = t.solBtn;
 
     document.getElementById('how-main-title').innerText = t.howMainTitle;
-    document.getElementById('how-main-sub').innerText = t.howMainSub;
     document.getElementById('step-1-p').innerText = t.step1P;
     document.getElementById('step-2-p').innerText = t.step2P;
     document.getElementById('step-3-p').innerText = t.step3P;
     document.getElementById('step-4-p').innerText = t.step4P;
     document.getElementById('how-btn').innerText = t.howBtn;
 
-    document.getElementById('ben-title').innerText = t.benTitle;
-    document.getElementById('ben-desc').innerText = t.benDesc;
-    document.getElementById('bp-1-t').innerText = t.bp1T;
-    document.getElementById('bp-1-d').innerText = t.bp1D;
-    document.getElementById('bp-2-t').innerText = t.bp2T;
-    document.getElementById('bp-2-d').innerText = t.bp2D;
-    document.getElementById('bp-3-t').innerText = t.bp3T;
-    document.getElementById('bp-3-d').innerText = t.bp3D;
-    document.getElementById('callout-text').innerText = t.calloutText;
-    document.getElementById('ben-btn').innerText = t.benBtn;
-
-    document.getElementById('cond-title').innerText = t.condTitle;
     document.getElementById('cond-subtitle').innerText = t.condSubtitle;
     document.getElementById('cond-text').innerText = t.condText;
 
